df62510db0db84c9ca97fc4bc92504fe7e1d7713
lautaroff <lauferreyraff@gmail.com>
2026-02-14 17:24:51 -0300

    Update site URL to match Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // Asegura que las rutas sean manejadas correctamente en producción
-  site: 'https://lautaro-ferreyra-portfolio.vercel.app', // Reemplaza con tu dominio real en Vercel
+  site: 'https://lautaroff.vercel.app', // Reemplaza con tu dominio real en Vercel
   base: '/',
 });
